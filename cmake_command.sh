cc24aee2dece39ba731122d0ceb8433fb25019f2
Rjunaidsh <102263509+Rjunaidsh@users.noreply.github.com>
2022-08-04 05:22:41 +0500

    Update cmake_command.sh

--- cmake_command.sh
+++ cmake_command.sh
@@ -1,7 +1,2 @@
-apt-get update
-rm /var/cache/apt/archives/lock
-rm /var/lib/dpkg/lock
-rm /var/lib/apt/lists/lock
-rm /var/cache/apt/archives/lock
-rm /var/lib/dpkg/lock
+sudo killall apt apt-get
 apt-get install -y g++ cmake build-essential 
